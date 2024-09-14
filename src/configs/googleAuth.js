681d73388978ccdab6338ea808a3489368eb5790
Wealth <shuaibganiyat20@gmail.com>
2024-09-14 22:26:13 +0100

    google

--- src/configs/googleAuth.js
+++ src/configs/googleAuth.js
@@ -1,12 +1,24 @@
+// import { google } from 'googleapis';
+// import fs from 'fs';
+
+// const credentials = JSON.parse(fs.readFileSync('src/secrets.json', 'utf8'));
+
+// // Set up the Google Auth
+// const auth = new google.auth.GoogleAuth({
+//     credentials,
+//     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+// });
+
+// export default auth;
+
 import { google } from 'googleapis';
-import fs from 'fs';
+import key from '../secrets.json' assert { type: 'json' };
 
-const credentials = JSON.parse(fs.readFileSync('src/secrets.json', 'utf8'));
+export const SHEET_ID = '1GwVIMV-YEDt5MTvCZrL1bRYwyE3izIY3adB4VGKNXsg';
 
-// Set up the Google Auth
-const auth = new google.auth.GoogleAuth({
-    credentials,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
+const client = new google.auth.JWT(key.client_email, null, key.private_key, [
+  'https://www.googleapis.com/auth/spreadsheets',
+]);
 
-export default auth;
+export const sheets = google.sheets({ version: 'v4', auth: client });
+
--- src/helpers/googleSheet.js
+++ src/helpers/googleSheet.js
@@ -1,45 +1,45 @@
-// import { SHEET_ID, sheets } from '../configs/googleAuth.js';
+import { SHEET_ID, sheets } from '../configs/googleAuth.js';
 
-
-// // Function to append data to the Google Sheet
-// export const appendToSheet = async (range, values) => {
-//     try {
-//         const response = await sheets.spreadsheets.values.append({
-//             spreadsheetId: SHEET_ID,
-//             range: range, // Use the passed range
-//             insertDataOption: 'INSERT_ROWS',
-//             valueInputOption: 'RAW',
-//             requestBody: {
-//                 values: values, // Use the passed values
-//             },
-//         });
-
-//         console.log('Data successfully appended:', response.data);
-//         return response.data;
-//     } catch (error) {
-//         console.error('Error appending data to Google Sheets:', error);
-//         throw new Error('Google Sheets API error: ' + error.message);
-//     }
-// };
-
-import { google } from 'googleapis';
-import auth from '../configs/googleAuth.js';
-
-const sheets = google.sheets({ version: 'v4', auth });
 
 // Function to append data to the Google Sheet
 export const appendToSheet = async (range, values) => {
-    const SPREADSHEET_ID = '1GwVIMV-YEDt5MTvCZrL1bRYwyE3izIY3adB4VGKNXsg';
-
     try {
-        await sheets.spreadsheets.values.append({
-            spreadsheetId: SPREADSHEET_ID,
-            range, // Sheet and range (e.g., 'Sheet1!A:D')
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId: SHEET_ID,
+            range: range, // Use the passed range
+            insertDataOption: 'INSERT_ROWS',
             valueInputOption: 'RAW',
-            resource: { values },
+            requestBody: {
+                values: values, // Use the passed values
+            },
         });
+
+        console.log('Data successfully appended:', response.data);
+        return response.data;
     } catch (error) {
+        console.error('Error appending data to Google Sheets:', error);
         throw new Error('Google Sheets API error: ' + error.message);
     }
 };
+
+// import { google } from 'googleapis';
+// import auth from '../configs/googleAuth.js';
+
+// const sheets = google.sheets({ version: 'v4', auth });
+
+// // Function to append data to the Google Sheet
+// export const appendToSheet = async (range, values) => {
+//     const SPREADSHEET_ID = '1GwVIMV-YEDt5MTvCZrL1bRYwyE3izIY3adB4VGKNXsg';
+
+//     try {
+//         await sheets.spreadsheets.values.append({
+//             spreadsheetId: SPREADSHEET_ID,
+//             range, // Sheet and range (e.g., 'Sheet1!A:D')
+//             valueInputOption: 'RAW',
+//             resource: { values },
+//         });
+//     } catch (error) {
+//         throw new Error('Google Sheets API error: ' + error.message);
+//     }
+// };
 

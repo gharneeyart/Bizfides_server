3cd51af1aba55f0d9fb6dce05420b7072da63dee
Wealth <shuaibganiyat20@gmail.com>
2024-09-14 07:13:00 +0100

    print

--- src/configs/googleAuth.js
+++ src/configs/googleAuth.js
@@ -7,9 +7,11 @@ dotenv.config();
 
 const clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
 const privateKey = process.env.GOOGLE_PRIVATE_KEY;
+console.log("Before Formatting", clientEmail, privateKey);
 
 // Replace escaped newline characters in private key
 const formattedPrivateKey = privateKey.replace(/\\n/g, '\n'); // Handle multiline keys
+console.log("After Formatting", formattedPrivateKey);
 
 export const SHEET_ID = process.env.SHEET_IDS;
 
